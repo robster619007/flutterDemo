7db536947bb67b639221a06a3977ed0449e8bc10
Robee Ghosh <rob93ghosh@gmail.com>
2020-07-20 11:02:26 +0400

    reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {}
 
 class MyApp extends StatelessWidget {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp();
   }
 }
