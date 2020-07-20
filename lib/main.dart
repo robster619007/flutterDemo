7ffb51e34f75d8c392029fdabd243f125046171d
Robee Ghosh <rob93ghosh@gmail.com>
2020-07-20 10:10:09 +0400

    added build widget within class MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+
 void main() {}
 
-class MyApp {}
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
