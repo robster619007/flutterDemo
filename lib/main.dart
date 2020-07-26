0ae730185fa13e5bbf9c07909775b2d350e0ead2
Robee Ghosh <rob93ghosh@gmail.com>
2020-07-26 14:40:11 +0400

    Centre aligned the container to show contents in the middle. Also created a back functionality for 2nd btn

--- lib/main.dart
+++ lib/main.dart
@@ -22,33 +22,35 @@ class _MyAppState extends State<MyApp> {
   }
 
   void buttonPressed2() {
-    print('Column 2 pressed');
+    setState(() {
+      _columnIndex = _columnIndex - 1;
+    });
+    print(_columnIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var columnName = ['Column 1', 'column 1', 'col1', 'COLUMN ONE'];
+    var columnName = [
+      'How do you do?',
+      'What\'s the question?',
+      'Where am I?',
+      'Who are you?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Demo'),
         ),
-        body: Row(
+        body: Column(
           children: [
-            Column(
-              children: [
-                Text(columnName[_columnIndex]),
-                RaisedButton(child: Text('Button1'), onPressed: buttonPressed1),
-                RaisedButton(
-                    child: Text('Button2'),
-                    onPressed: () => print('btn 2 for col 1 clicked')),
-                RaisedButton(
-                    child: Text('Button3'),
-                    onPressed: () {
-                      print('btn3 for col1 clicked');
-                    }),
-              ],
-            ),
+            Question(columnName[_columnIndex]),
+            RaisedButton(child: Text('Button1'), onPressed: buttonPressed1),
+            RaisedButton(child: Text('Button2'), onPressed: buttonPressed2),
+            RaisedButton(
+                child: Text('Button3'),
+                onPressed: () {
+                  print('btn3 for col1 clicked');
+                }),
           ],
         ),
       ),
